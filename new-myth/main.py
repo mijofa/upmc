d26c586156d6dd8605cfaf62c661d1395dbc9b22
Mike Fabre <mijofa+github@fabre.id.au>
2012-03-27 14:21:30 +1100

    Code cleanup

--- new-myth/main.py
+++ new-myth/main.py
@@ -6,11 +6,100 @@ try: # I would like this to run on Android as well, this section is needed for t
 	android.map_key(android.KEYCODE_BACK, pygame.K_ESCAPE)
 except ImportError: android = None
 
+global screenupdates
+screenupdates = []
+global running
+running = True
+
+
 def quit():
 	global running
 	running = False
 	print 'User quit... Bye.'
 	pygame.quit()
+
+class textmenu():
+	menuitems = [('Videos', "Videos menu has not been coded yet."), ('Extra item', 'testing'), ('Quit', quit)] # Update this with extra menu items, this should be a list containing one tuple per item, the tuple should contain the menu text and the function that is to be run when that option gets selected.
+	clickables = {}
+	realmenuitems = []
+	selected = None
+	def __init__(self):
+		global screenupdates
+		# This puts the menu items on the screen and populates the necessary variables for selecting the items later.
+		itemheight = screen.get_height()/len(self.menuitems)
+		itemnum = -1
+		for item in self.menuitems:
+			itemnum += 1
+			text = font.render(item[0], 1, (255,255,255))
+#			textpos = text.get_rect(centerx=screen.get_width()/2,centery=(itemheight*itemnum)+(itemheight/2))
+			textpos = text.get_rect(centerx=screen.get_width()/2,centery=(itemheight*itemnum)+(itemheight/2))
+			self.clickables.update({tuple(textpos[0:4]): itemnum})#(text, item[1])})
+			self.realmenuitems.append((text, item[1], textpos))
+			screen.blit(text, textpos)
+			screenupdates.append(textpos)
+		pygame.display.update(screenupdates)
+		screenupdates = []
+	def mouseselect(self, mousepos):
+		global screenupdates
+		# This will highlight an item based on the current mouse location, this should be called anytime the mouse moves.
+		item = pygame.Rect(mousepos[0],mousepos[1],0,0).collidedict(self.clickables)
+		if item and self.realmenuitems[item[1]] != self.selected:
+			if self.selected:
+				screen.blit(background, (0,0))
+				screen.blit(self.selected[0], self.selected[2])
+				screenupdates.append(self.selected[2])
+			self.selected = self.realmenuitems[item[1]]
+			s = pygame.Surface(self.selected[2][2:4], pygame.SRCALPHA)
+			s.fill((0,0,0,50))
+			s.blit(self.selected[0], (0,0))
+			screen.blit(s, self.selected[2])
+			screenupdates.append(self.selected[2])
+			pygame.display.update(screenupdates)
+			screenupdates = []
+		elif not item and self.selected != None:
+			screen.blit(background, (0,0))
+			screen.blit(self.selected[0], self.selected[2])
+			screenupdates.append(self.selected[2])
+			pygame.display.update(screenupdates)
+			screenupdates = []
+			self.selected = None
+		return self.selected
+	def keyselect(self, direction):
+		global screenupdates
+		# This will highlight the next/previous item based on keypress, this should be called with True for next (down), and False for previous (up).
+		if not self.selected and direction == False:
+			self.selected = self.realmenuitems[-1]
+		elif not self.selected and direction == True:
+			self.selected = self.realmenuitems[0]
+		elif self.selected:
+			screen.blit(background, (0,0))
+			screen.blit(self.selected[0], self.selected[2])
+			screenupdates.append(self.selected[2])
+			if direction == False:
+				try: self.selected = self.realmenuitems[self.realmenuitems.index(self.selected)-1]
+				except IndexError: self.selected = self.realmenuitems[-1]
+			elif direction == True:
+				try: self.selected = self.realmenuitems[self.realmenuitems.index(self.selected)+1]
+				except IndexError: self.selected = self.realmenuitems[0]
+			print self.realmenuitems.index(self.selected)
+		s = pygame.Surface(self.selected[2][2:4], pygame.SRCALPHA)
+		s.fill((0,0,0,50))
+		s.blit(self.selected[0], (0,0))
+		screen.blit(s, self.selected[2])
+		screenupdates.append(self.selected[2])
+		pygame.display.update(screenupdates)
+		screenupdates = []
+	def action(self, args = None):
+		# This will action the highlighted item.
+		if type(self.selected[1]) == str:
+			print self.selected[1]
+		else:
+			if args != None:
+				return self.selected[1](args)
+			else:
+				return self.selected[1]()
+##### End class textmenu()
+
 
 ## The Pygame modules need to be initialised before they can be used.
 ### The Pygame docs say to just initialise *everything* at once, I thing this is wasteful and am only initialising the bits I'm using.
@@ -20,11 +109,6 @@ pygame.font.init()
 pygame.display.init()
 #pygame.transform.init() # Doesn't have an '.init()' funtion.
 #pygame.event.init() # Doesn't have an '.init()' funtion.
-
-global screenupdates
-screenupdates = []
-global running
-running = True
 
 screen = pygame.display.set_mode((640,480)) # Create a new window.
 try: background = pygame.transform.scale(pygame.image.load('Retro/ui/background.png'), screen.get_size()).convert() # Resize the background image to fill the window.
@@ -41,22 +125,33 @@ if android:
 else:
 	font = pygame.font.Font(pygame.font.match_font(u'trebuchetms'), 36) # Might want to use a non-MS font.
 
-class textmenu():
-	menuitems = [('Videos', "Videos menu has not been coded yet."), ('Extra item', 'testing'), ('Quit', quit)]
-	clickables = {}
-	realmenuitems = []
-	def __init__(self):
-		itemheight = screen.get_height()/len(menuitems)
-		itemnum = 0
-		for item in self.menuitems:
-			itemnum += 1
-			text = font.render(item[0], 1, (255,255,255))
-			textpos = text.get_rect(centerx=screen.get_width()/2,centery=(itemheight*itemnum)+(itemheight/2))
-			self.clickables.update({tuple(textpos[0:4]): itemnum})#(text, item[1])})
-			self.realmenuitems.append((text, item[1], textpos))
-			screen.blit(text, textpos)
-			screenupdates.append(textpos)
-		pygame.display.update(screenupdates)
-		screenupdates = []
-		pygame.event.set_allowed([pygame.MOUSEMOTION,pygame.MOUSEBUTTONDOWN,pygame.MOUSEBUTTONUP,pygame.KEYDOWN,pygame.KEYUP])
-	
+menu = textmenu()
+
+## These should avoid going through the loop unnecessarily (and wasting resources) when there is events that I'm not going to use anyway.
+pygame.event.set_allowed(None) # This says to not put *any* events into the event queue.
+pygame.event.set_allowed([pygame.MOUSEMOTION,pygame.MOUSEBUTTONDOWN,pygame.MOUSEBUTTONUP,pygame.KEYDOWN]) # This says to put the events I want to see into the event queue, this needs to be updated anytime I want to monitor more events.
+while running == True:
+	event = pygame.event.wait()
+	if event.type == pygame.QUIT:
+		running = 0
+		pygame.quit()
+	elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+		released = False
+		item = menu.mouseselect(event.pos)
+		if item:
+			while released != True:
+				event = pygame.event.wait()
+				if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+					released = True
+					if menu.mouseselect(event.pos) == item:
+						menu.action()
+					else:
+						print 'User changed their mind and released the button elsewhere.'
+	elif event.type == pygame.MOUSEMOTION:
+		menu.mouseselect(event.pos)
+	elif event.type == pygame.KEYDOWN and (event.key == pygame.K_UP or event.key == pygame.K_DOWN):
+		menu.keyselect(event.key==pygame.K_DOWN) # This will call keyselect(False) if K_UP is pressed, and keyselect(True) if K_DOWN is pressed.
+	elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+		menu.action()
+	else:
+		print event
