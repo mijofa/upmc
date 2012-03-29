c32ddc5ed2fd06846cf79aa7e7c34f0606690ad0
Mike Fabre <mijofa+github@fabre.id.au>
2012-03-29 17:06:03 +1100

    Additions to filemenu()

--- new-myth/main.py
+++ new-myth/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+import os
+import magic
 import pygame
 try: # I would like this to run on Android as well, this section is needed for that to work.
 	import android
@@ -16,18 +18,18 @@ running = True
 
 def userquit():
 	pygame.event.post(pygame.event.Event(pygame.QUIT, {}))
+	return pygame.event.Event(pygame.QUIT, {})
 
 class textmenu():
-	menuitems = [('Videos', "Videos menu has not been coded yet."), ('Extra item', 'testing'), ('Quit', userquit)] # Update this with extra menu items, this should be a list containing one tuple per item, the tuple should contain the menu text and the function that is to be run when that option gets selected.
 	clickables = {}
 	realmenuitems = []
 	selected = None
-	def __init__(self):
+	def __init__(self, menuitems):
 		global screenupdates
 		# This puts the menu items on the screen and populates the necessary variables for selecting the items later.
-		itemheight = screen.get_height()/len(self.menuitems)
+		itemheight = screen.get_height()/len(menuitems)
 		itemnum = -1
-		for item in self.menuitems:
+		for item in menuitems:
 			itemnum += 1
 			text = font.render(item[0], 1, (255,255,255))
 #			textpos = text.get_rect(centerx=screen.get_width()/2,centery=(itemheight*itemnum)+(itemheight/2))
@@ -103,6 +105,9 @@ class filemenu():
 	itemsinfo = {}
 	items = []
 	cwd = './'
+	def __init__(self):
+		self.builditems()
+		self.render()
 	def find(self, directory = cwd, filetype = ''):
 		dirs = []
 		files = []
@@ -132,8 +137,9 @@ class filemenu():
 			mime.load()
 			out = []
 			for f in os.listdir(directory):
-				if mime.file(f).split(';')[0] == filetype:
-					out.append(f)
+				if not os.path.isdir(directory+'/'+f):
+					if mime.file(directory+'/'+f).split(';')[0] == filetype:
+						out.append(directory+'/'+f)
 			out.sort()
 			return out
 		elif not filetype.__contains__('/'):
@@ -141,8 +147,9 @@ class filemenu():
 			mime.load()
 			out = []
 			for f in os.listdir(directory):
-				if mime.file(f).split('/')[0] == filetype:
-					out.append(f)
+				if not os.path.isdir(directory+'/'+f):
+					if mime.file(directory+'/'+f).split('/')[0] == filetype:
+						out.append(directory+'/'+f)
 			out.sort()
 			return out
 		else:
@@ -152,51 +159,94 @@ class filemenu():
 		self.itemsinfo = {}
 		self.items = []
 		for item in self.find(filetype='directory'):
-			itemnum += 1
-			self.items.append(item)
-			if not self.itemsinfo[item]:
-				self.itemsinfo[item] = {}
-			self.itemsinfo[item]['file'] = False
-			self.itemsinfo[item]['title'] = item
-			self.itemsinfo[item]['itemnum'] = itemnum
-			self.itemsinfo[item]['filename'] = item + '/'
-			for thumb in self.find(item, 'image'):
-				if thumb.startswith('folder.'):
-					items[item]['thumb'] = item + '/' + thumb
-					break
-		for filename in self.find(filetype='video'):
-			item = filename.rpartition('.')[0]
-			itemnum += 1
-			self.items.append(item)
-			if not self.itemsinfo[item]:
-				self.itemsinfo[item] = {}
-			self.itemsinfo[item]['file'] = True
-			self.itemsinfo[item]['title'] = item
-			self.itemsinfo[item]['itemnum'] = itemnum
-			self.itemsinfo[item]['filename'] = filename
+			if not item.startswith('.'):
+				itemnum += 1
+				self.items.append(item)
+				if not self.itemsinfo.has_key(item):
+					self.itemsinfo[item] = {}
+				self.itemsinfo[item]['file'] = False
+				self.itemsinfo[item]['title'] = item
+				self.itemsinfo[item]['itemnum'] = itemnum
+				self.itemsinfo[item]['filename'] = item + '/'
+				for thumb in self.find(item, 'image'):
+					if thumb.startswith('folder.'):
+						items[item]['thumb'] = item + '/' + thumb
+						break
+		print itemnum
+		for filename in self.find(filetype='file'):
+			if not filename.startswith('.'):
+				item = filename.rpartition('.')
+				if item[1] == '.':
+					item = item[0]
+				else:
+					item = item[2]
+				itemnum += 1
+				self.items.append(item)
+				if not self.itemsinfo.has_key(item):
+					self.itemsinfo[item] = {}
+				self.itemsinfo[item]['file'] = True
+				self.itemsinfo[item]['title'] = item
+				self.itemsinfo[item]['itemnum'] = itemnum
+				self.itemsinfo[item]['filename'] = filename
+		print itemnum
 		for filename in self.find(filetype='image'):
-			item = filename.rpartition('.')[0]
-			if self.itemsinfo[item]:
-				self.itemsinfo[item]['thumb'] = filename
+			if not filename.startswith('.'):
+				item = filename.rpartition('.')[0]
+				if item[1] == '.':
+					item = item[0]
+				else:
+					item = item[2]
+				if self.itemsinfo.has_key(item):
+					self.itemsinfo[item]['thumb'] = filename
+		print itemnum
+		print self.itemsinfo
 	def render(self, directory = cwd):
 		global screenupdates
 		screen.blit(background, (0,0))
 		pygame.display.update()
-		itemheight = screen.get_height()/4
-		itemvdist = screen.get_height()/5
-		itemwidth = screen.get_width()/6
-		itemhdist = screen.get_width/7
+		itemheight = screen.get_height()/7
+		itemydist = screen.get_width()/6
+		itemwidth = screen.get_width()/5
+		itemxdist = screen.get_height()/4
+		print itemheight, itemydist, itemwidth, itemxdist
+		from time import sleep
 		for item in self.items:
-			if itemsinfo[item][thumb]:
-				s = pygame.image.load(itemsinfo[item][thumb])
-			else:
-				s = font.render(itemsinfo[item]['title'], 1, (255,255,255))
-			r = s.get_rect().fit((0,0,itemwidth,itemheight))
-			self.itemsinfo[item]['surface'] = pygame.trasform.scale(s, (r[2], r[3])).convert()
-
-				
-
+#			if self.itemsinfo[item].has_key('thumb'):
+#				s = pygame.image.load(self.itemsinfo[item]['thumb'])
+#			else:
+			f = font.render(self.itemsinfo[item]['title'], 1, (255,255,255))
+			r = f.get_rect().fit((0,0,itemwidth,itemheight))
+			s = pygame.Surface((itemwidth,itemheight), pygame.SRCALPHA)
+			s.fill((0,0,0,50))
+			f = pygame.transform.scale(f, (r[2], r[3]))
+			s.blit(f, (0,0))
+			self.itemsinfo[item]['surface'] = s #pygame.transform.scale(s, (r[2], r[3]))
+#			screen.blit(pygame.transform.scale(s, (r[2], r[3])), s.get_rect(centerx=screen.get_width()/2,centery=screen.get_height()/2))
+#			pygame.display.update()
+#			if int(self.itemsinfo[item]['itemnum']/2) != 0:
+#				centerx=((itemydist*self.itemsinfo[item]['itemnum'])/int(self.itemsinfo[item]['itemnum']/2))+(itemydist/2)-(itemydist*2)
+#				print int(self.itemsinfo[item]['itemnum']/2), itemydist*self.itemsinfo[item]['itemnum'], (itemydist*self.itemsinfo[item]['itemnum'])/int(self.itemsinfo[item]['itemnum']/2)
+			centerx=(itemxdist*(self.itemsinfo[item]['itemnum']-(int(self.itemsinfo[item]['itemnum']/4)*4)))+(itemxdist)
+#			else:
+#				centerx=(itemydist*self.itemsinfo[item]['itemnum'])+(itemydist/2)
+			centery=(itemydist*int(self.itemsinfo[item]['itemnum']/4))+(itemydist/2)
+			print centery
+#			print centery, centerx
+			self.itemsinfo[item]['screenpos'] = self.itemsinfo[item]['surface'].get_rect(centerx=centerx,centery=centery)# =centerx)
+#			print itemxdist, int(self.itemsinfo[item]['itemnum']/4), (itemxdist/2), (itemydist*self.itemsinfo[item]['itemnum'])+(itemydist/2)
+#			print self.itemsinfo[item]['itemnum'], self.itemsinfo[item]['title'], self.itemsinfo[item]['surface'], self.itemsinfo[item]['screenpos']
+			screen.blit(self.itemsinfo[item]['surface'], self.itemsinfo[item]['screenpos'])
+			screenupdates.append(self.itemsinfo[item]['screenpos'])
+			pygame.display.update(screenupdates)
+			screenupdates = []
+#			try: sleep(1)
+#			except KeyboardInterrupt:
+#				event = userquit()
+#				break
 ##### End class filemenu()
+
+def vidsmenu():
+	filemenu()
 
 ## The Pygame modules need to be initialised before they can be used.
 ### The Pygame docs say to just initialise *everything* at once, I think this is wasteful and am only initialising the bits I'm using.
@@ -208,6 +258,7 @@ pygame.display.init()
 #pygame.event.init() # Doesn't have an '.init()' funtion.
 
 screen = pygame.display.set_mode((640,480)) # Create a new window.
+#screen = pygame.display.set_mode((800,600)) # Create a new window.
 try: background = pygame.transform.scale(pygame.image.load('Retro/ui/background.png'), screen.get_size()).convert() # Resize the background image to fill the window.
 except: # Failing that (no background image?) just create a completely blue background.
 	background = pygame.Surface(screen.get_size()).convert() 
@@ -222,14 +273,16 @@ if android:
 else:
 	font = pygame.font.Font(pygame.font.match_font(u'trebuchetms'), 36) # Might want to use a non-MS font.
 
-menu = textmenu()
+menuitems = [('Videos', vidsmenu), ('Extra item', 'testing'), ('Quit', userquit)] # Update this with extra menu items, this should be a list containing one tuple per item, the tuple should contain the menu text and the function that is to be run when that option gets selected.
+menu = textmenu(menuitems)
 
 ## These should avoid going through the loop unnecessarily (and wasting resources) when there is events that I'm not going to use anyway.
 pygame.event.set_allowed(None) # This says to not put *any* events into the event queue.
 pygame.event.set_allowed([pygame.QUIT])
 pygame.event.set_allowed([pygame.MOUSEMOTION,pygame.MOUSEBUTTONDOWN,pygame.MOUSEBUTTONUP,pygame.KEYDOWN]) # This says to put the events I want to see into the event queue, this needs to be updated anytime I want to monitor more events.
 while running == True:
-	event = pygame.event.wait()
+	try: event = pygame.event.wait()
+	except KeyboardInterrupt: event = userquit()
 	if event.type == pygame.QUIT:
 		running = False
 		pygame.quit()
@@ -252,4 +305,5 @@ while running == True:
 	elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
 		menu.action()
 	else:
-		print event
+		pass
+
