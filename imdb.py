ba6333ae168d49ee30a6dfecb9db13a5177ceb1a
Mike Fabre <mijofa+github@fabre.id.au>
2012-05-09 16:28:42 +1000

    Made lots and lots of changes to the IMDB script, mostly for TV shows/episodes, but some that effect everything, such as using the IMDB id already stored in the .info file.

--- imdb.py
+++ imdb.py
@@ -13,14 +13,16 @@ import ConfigParser
 Config = ConfigParser.ConfigParser()
 
 tv = False
-forceimdb = False
+noforceimdb = False
 forceposter = False
 n = 1
 while n < len(sys.argv):
-  if sys.argv[n] == '--force' or sys.argv[n] == '-f':
+  if sys.argv[n] == '--force-poster' or sys.argv[n] == '-f':
     sys.argv.pop(n)
-    forceimdb = True
     forceposter = True
+  if sys.argv[n] == '--no-overwrite':
+    sys.argv.pop(n)
+    noforceimdb = True
   elif sys.argv[n] == '--television' or sys.argv[n] == '-tv':
     sys.argv.pop(n)
     tv = True
@@ -35,18 +37,28 @@ if path == '':
 sys.stdout.write('Processing '+path+'/'+title+', ')
 sys.stdout.flush()
 
-if forceimdb == False and os.path.isfile('.'.join(sys.argv[1].split('.')[:-1])+'.info'):
+if noforceimdb == True and os.path.isfile(path+title+'.info'):
   sys.stdout.write('.info file already exists, ')
   sys.stdout.flush()
   Config.read(path+'/'+title+'.info')
 else:
   sys.stdout.write('IMDB info, ')
   sys.stdout.flush()
+  IMDBid = 0
+  if os.path.isfile(path+'/'+title+'.info'):
+    Config.read(path+'/'+title+'.info')
+    if Config.has_section('IMDB') and Config.has_option('IMDB', 'id'):
+      IMDBid = Config.getint('IMDB', 'id')
+    for section in Config.sections():
+      if not section == 'local':
+        Config.remove_section(section)
   Config.add_section('IMDB')
   IMDB = imdb.IMDb('http')
-  if tv == True:
+  if not IMDBid == 0:
+    movie = IMDB.get_movie(IMDBid)
+  elif tv == True:
     showname = None
-    seasonnum = 0
+    seasonnum = None
     episodenum = 0
     if path.split('/')[-1].lstrip('S').isdigit():
       showname = path.split('/')[-2]
@@ -62,39 +74,49 @@ else:
       seasonnum = int(title.lstrip('S').split('E')[0])
       if title.split('E')[1].split(' ')[0].isdigit():
         episodenum = int(title.split('E')[1].split(' ')[0])
+    elif ' - ' in title:
+      showname = title.split(' - ')[0]
+      temptitle = title.split(' - ')[1]
+      if temptitle.lstrip('E').isdigit():
+        episodenum = int(temptitle.lstrip('E'))
+      elif temptitle.startswith('S') and temptitle.lstrip('S').split('E')[0].isdigit():
+        seasonnum = int(temptitle.lstrip('S').split('E')[0])
+        if temptitle.split('E')[1].split(' ')[0].isdigit():
+          episodenum = int(temptitle.split('E')[1].split(' ')[0])
     if showname == None:
       raise Exception, 'Unable to determine TV show name.'
     else:
-      search = IMDB.search_movie(showname)
-      for result in search:
-        if result['kind'] == 'tv series':
-          show = IMDB.get_movie(result.getID())
-          IMDB.update(show, 'episodes')
-          try: movie = show['episodes'][seasonnum][episodenum]
-          except KeyError: raise KeyError, imdb.__file__+': unable to find episode S%02dE%02d' % (seasonnum,episodenum)
-          if not Config.has_section('local'): Config.add_section('local')
-          Config.set('local', 'title', ' - '.join([showname, 'S%02dE%02d'%(seasonnum,episodenum), movie['title']]))
-          print Config.get('local', 'title')
-          break
-#    raise Exception, "random"
+      if path.split('/')[-1].lstrip('S').isdigit() or (path.split('/')[-1].startswith('Season ') and ' '.join(path.split('/')[-1].split(' ')[1:]).isdigit()):
+        showrootpath = path.split('/')[-1]
+      if os.path.isfile(showrootpath+'/'+'.IMDBid.txt'):
+        show = IMDB.get_movie(int(open(showrootpath+'/'+'.IMDBid.txt', 'r').readline()))
+      else:
+        search = IMDB.search_movie(showname)
+        for result in search:
+          if result['kind'] == 'tv series':
+            show = IMDB.get_movie(result.getID())
+            open(showrootpath+'/'+'.IMDBid.txt', 'w').writeline(str(result.getID()))
+            break
+      IMDB.update(show, 'episodes')
+      if seasonnum == None:
+        try: movie = IMDB.get_movie(show['episodes'][episodenum].getID())
+        except KeyError: raise KeyError, imdb.__file__+': unable to find episode S%02dE%02d' % (seasonnum,episodenum)
+      else:
+        try: movie = IMDB.get_movie(show['episodes'][seasonnum][episodenum].getID())
+        except KeyError: raise KeyError, imdb.__file__+': unable to find episode S%02dE%02d' % (seasonnum,episodenum)
+      if not Config.has_section('local'): Config.add_section('local')
+      Config.set('local', 'title', ' - '.join([showname, 'S%02dE%02d'%(seasonnum,episodenum), movie['title']]))
   else:
     search = IMDB.search_movie(title)
     movie = IMDB.get_movie(search[0].getID())
-#  if sys.argv[2] == '-tv':
-#    print movie.get('episodes')
-#    raise Exception, "testing"
-  Config.set('IMDB', 'ID', search[0].getID())
+  Config.set('IMDB', 'id', movie.getID())
   for key in movie.keys():
     item = movie[key]
     if type(item) == list:
       itemstring = ''
       for listitem in item:
-  #     try:
         if not listitem == '':
           itemstring += str(listitem)+' | '
-  #     except:
-  #       print type(listitem), dir(listitem), listitem.encode('utf8')
-  #       raise Exception, "Bye bye"
       if not itemstring.rstrip(', ') == '':
         Config.set('IMDB',key,itemstring.rstrip(', '))
     elif not str(item) == '':
