c8d842b6eab9525151da7d4a9979b3ad705031b2
Mike Fabre <mijofa+github@fabre.id.au>
2013-05-28 16:04:22 +1000

    added vlc.py and imdb.py to setup.py. Added auto-updating of upstream vlc.py in the setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
+
+import urllib2
+url = urllib2.urlopen("http://git.videolan.org/?p=vlc/bindings/python.git;a=blob_plain;f=generated/vlc.py;hb=HEAD")
+data = url.read()
+url.close()
+vlc_file = open("vlc.py", 'r+')
+if data != vlc_file.read():
+  vlc_file.seek(0)
+  vlc_file = open("vlc.py", 'w')
+  vlc_file.write(data)
+  vlc_file.flush()
+  vlc_file.close()
+
 from distutils.core import setup
 setup(name='upmc',
       version='1.0',
       packages=['upmc'],
+      py_modules=['vlc', 'imdb'],
       )
